8a5161ee9eae8390dfad369afb1438ae63ec732c
Paul Arnold <paul@pjarnold.com>
2015-12-23 12:15:47 +0000

    Add Events Custom Post Type

--- wp-content/themes/twentysixteen-child/functions.php
+++ wp-content/themes/twentysixteen-child/functions.php
@@ -5,3 +5,40 @@ function theme_enqueue_styles() {
 
 }
 ?>
+
+<?php
+/** Custom post type for Events **/
+add_action('init', 'events_init');
+
+function events_init() {
+	$labels = array(
+					'name' => _x('Events', 'post type general name'),
+					'singular_name' => _x('Event', 'post type singular name'),
+					'add_new' => _x('Add New', 'event'),
+					'add_new_item' => __('Add New Event'),
+					'edit_item' => __('Edit This Event'),
+					'new_item' => __('New Event'),
+					'view_item' => __('View This Event'),
+					'search_items' => __('Search Events'),
+					'not_found' => __('No Event found'),
+					'not_found_in_trash' => __('No Event found in Trash'),
+					'menu_name' => _x('Events')
+					);
+	$args = array(
+				  'labels' => $labels,
+				  'public' => true,
+				  'publicly_queryable' => true,
+				  'query_var' => true,
+				  'rewrite' => array('slug' => 'events'),
+				  'capability_type' => 'post',
+				  'hierarchical' => true,
+				  'taxonomies' => array('category'),				  
+				  'menu_postition' => null,
+				  'has_archive' => true,
+				  'show_in_menu' => true,
+				  'supports' => array('title','category','editor','excerpt','custom-fields','thumbnail','page-attributes'),
+				  'show_in_nav_menus' => true
+				  );
+	register_post_type('events',$args);
+}
+?>
