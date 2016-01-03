f5ec0cb95b8e2d4d626b8db64e632ce688c36a60
Paul Arnold <paul@pjarnold.com>
2016-01-03 09:08:46 +0000

    Update to header image

--- wp-content/themes/twentysixteen-child/header.php
+++ wp-content/themes/twentysixteen-child/header.php
@@ -88,9 +88,7 @@
 					$custom_header_sizes = apply_filters( 'twentysixteen_custom_header_sizes', '(max-width: 709px) 85vw, (max-width: 909px) 81vw, (max-width: 1362px) 88vw, 1200px' );
 				?>
 				<div class="header-image">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-						<img src="<?php header_image(); ?>" srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>" sizes="<?php echo esc_attr( $custom_header_sizes ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
-					</a>
+					<img src="<?php header_image(); ?>" srcset="<?php echo esc_attr( wp_get_attachment_image_srcset( get_custom_header()->attachment_id ) ); ?>" sizes="<?php echo esc_attr( $custom_header_sizes ); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
 				</div>
 			<?php endif; // End header image check. ?>
             <?php if (is_front_page()) {
@@ -103,7 +101,7 @@
 		</header><!-- .site-header -->
         
 		<?php if (is_front_page()) {
-			echo '<div class="home-banner"><img src="/wp-content/themes/twentysixteen-child/images/home-banner.png" alt="Mayfield Festival Hero" style="display: none;" /></div>';
+			echo '<div class="header-image"><img src="/wp-content/uploads/2016/01/header-image_1200x280.png" alt="Mayfield Festival Hero" /></div>';
 		} ?>
         
 
