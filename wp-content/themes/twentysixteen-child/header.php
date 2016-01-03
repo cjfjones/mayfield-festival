190645324d9f2867b21d6f7ec3394c3090c2bd80
Paul Arnold <paul@pjarnold.com>
2016-01-03 09:16:18 +0000

    Display none

--- wp-content/themes/twentysixteen-child/header.php
+++ wp-content/themes/twentysixteen-child/header.php
@@ -101,7 +101,7 @@
 		</header><!-- .site-header -->
         
 		<?php if (is_front_page()) {
-			echo '<div class="header-image"><img src="/wp-content/uploads/2016/01/header-image-piano_1200x280.png" alt="Mayfield Festival Hero" /></div>';
+			echo '<div class="header-image" style="display: none;"><img src="/wp-content/uploads/2016/01/header-image-piano_1200x280.png" alt="Mayfield Festival Hero" /></div>';
 		} ?>
         
 
