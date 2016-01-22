0c1cb724814ce617e35f00cd3281f4c1c2690837
cjfjones <chrisjones@posturite.co.uk>
2016-01-22 15:04:39 +0000

    Add metadata to side of events page

--- wp-content/themes/twentysixteen-child/single-events.php
+++ wp-content/themes/twentysixteen-child/single-events.php
@@ -22,8 +22,8 @@ get_header(); ?>
 			$tmp_eventbookingurl = get_post_meta($post->ID, 'Booking url', true);							
 			// End custom meta data (PJA)
 
-		// Include the single post content template.
-			//get_template_part( 'template-parts/content', 'single' );
+			// Include the single post content template.
+			get_template_part( 'template-parts/content', 'single' );
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
@@ -51,7 +51,22 @@ get_header(); ?>
 		endwhile;
 		?>
         
-     
+        <!-- Add custom meta data (PJA) -->
+        <footer class="entry-footer event-entry">
+			<?php if (get_post_meta($post->ID, 'Date', true)) : ?>
+            <span class="date"><?php echo $tmp_eventdate ?></span>
+            <?php endif; ?>
+            <?php if (get_post_meta($post->ID, 'Location', true)) : ?>
+			<span class="location"><?php echo $tmp_eventlocation ?></span>
+			<?php endif; ?>
+            <?php if (get_post_meta($post->ID, 'Ticket Prices', true)) : ?>
+            <span class="ticket-price">Tickets - <?php echo $tmp_eventprice ?></span>
+			<?php endif; ?>
+            <?php if (get_post_meta($post->ID, 'Booking url', true)) : ?>
+            <span class="booking-url"><a href="<?php echo $tmp_eventbookingurl ?>">Book now</a></span>
+            <?php endif; ?>
+        </footer>
+		<!-- End custom meta data (PJA) -->
         
 	</main><!-- .site-main -->
 
---
+++ wp-content/themes/twentysixteen-child/template-parts/content-single.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * The template part for displaying single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<?php twentysixteen_excerpt(); ?>
+
+	<?php twentysixteen_post_thumbnail(); ?>
+
+	<div class="entry-content">
+		<?php
+			the_content();
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentysixteen' ) . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentysixteen' ) . ' </span>%',
+				'separator'   => '<span class="screen-reader-text">, </span>',
+			) );
+
+			if ( '' !== get_the_author_meta( 'description' ) ) {
+				get_template_part( 'template-parts/biography' );
+			}
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+        
+        <?php
+        	// Add custom meta data (PJA)
+			$tmp_eventdate = get_post_meta($post->ID, 'Date', true);							
+			$tmp_eventlocation = get_post_meta($post->ID, 'Location', true);							
+			$tmp_eventprice = get_post_meta($post->ID, 'Ticket Prices', true);							
+			$tmp_eventbookingurl = get_post_meta($post->ID, 'Booking url', true);							
+			// End custom meta data (PJA)
+           ?>
+            
+       <?php echo $tmp_eventdate; ?><br />
+       <?php echo $tmp_eventlocation; ?><br />
+       <?php echo $tmp_eventprice; ?><br />
+       <?php echo $tmp_eventbookingurl; ?><br />
+       
+		<?php twentysixteen_entry_meta(); ?>
+		<?php
+			edit_post_link(
+				sprintf(
+					/* translators: %s: Name of current post */
+					__( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+					get_the_title()
+				),
+				'<span class="edit-link">',
+				'</span>'
+			);
+		?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
