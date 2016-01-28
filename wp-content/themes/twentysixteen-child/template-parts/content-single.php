8c18862bfb286a388f6e8107a77ac2181071f6d7
cjfjones <chrisjones@posturite.co.uk>
2016-01-28 12:59:08 +0000

    missing quote on url

--- wp-content/themes/twentysixteen-child/template-parts/content-single.php
+++ wp-content/themes/twentysixteen-child/template-parts/content-single.php
@@ -50,7 +50,7 @@
        <span class="event-details event-date"><?php echo $tmp_eventdate; ?></span>
        <span class="event-details event-location"><?php echo $tmp_eventlocation; ?></span>
        <span class="event-details event-price"><?php echo $tmp_eventprice; ?></span>
-       <span><a class="event-button" href="<?php echo $tmp_eventbookingurl; ?> target="_blank">Book now</a></span>
+       <span><a class="event-button" href="<?php echo $tmp_eventbookingurl; ?>" target="_blank">Book now</a></span>
        
 		<?php twentysixteen_entry_meta(); ?>
 		<?php
